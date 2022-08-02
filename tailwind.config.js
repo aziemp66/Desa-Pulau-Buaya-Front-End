4f7e5b06bb99efed864a4ce84212450a164e57ff
Azie Melza Pratama <aziemp66@gmail.com>
2022-08-03 00:37:27 +0700

    Adding Color Pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       fontFamily: {
         "noto-sans": ["Noto Sans"],
       },
+      colors: {
+        "blue-dark": "#2b6474",
+        "blue-light": "#759ba6",
+        "cream-dark": "#bfb6a4",
+        "cream-light": "#f5f5ef",
+      },
     },
   },
   plugins: [],
