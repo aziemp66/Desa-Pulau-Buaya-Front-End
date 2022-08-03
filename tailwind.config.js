1aea163fe526ae6e2ef8fea9f062a473748ed055
Azie Melza Pratama <aziemp66@gmail.com>
2022-08-03 23:26:21 +0700

    Successfully Adding Images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      "noto-sans": ["Noto Sans", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
     extend: {
-      fontFamily: {
-        "noto-sans": ["Noto Sans"],
-        inter: ["Inter"],
-      },
       colors: {
         "blue-dark": "#2b6474",
         "blue-light": "#759ba6",
