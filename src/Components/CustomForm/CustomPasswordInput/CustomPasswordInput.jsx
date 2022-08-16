647ffd65cc65f189a642a742c6d479f3ee23edfc
FadhilAF <falfatih3421@gmail.com>
2022-08-16 22:44:36 +0700

    changing button in form

--- src/Components/CustomForm/CustomPasswordInput/CustomPasswordInput.jsx
+++ src/Components/CustomForm/CustomPasswordInput/CustomPasswordInput.jsx
@@ -5,23 +5,14 @@ const customPasswordInput = (props) => (
     <p className="mb-1 text-sm">{props.name}</p>
     <div className="flex-1 relative">
       <div className="[&>*>*]:w-6 [&>*>*]:h-6 [&>*>*]:fill-slate-400 absolute inset-y-0 right-0 mr-2 flex items-center">
-        {props.showState ? (
-          <button
-            onClick={() => {
-              props.setShowState(false);
-            }}
-          >
-            <AiFillEye />
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              props.setShowState(true);
-            }}
-          >
-            <AiFillEyeInvisible />
-          </button>
-        )}
+        <button
+          type="button"
+          onClick={() => {
+            props.setShowState(!props.showState);
+          }}
+        >
+          {props.showState ? <AiFillEye /> : <AiFillEyeInvisible />}
+        </button>
       </div>
       <input
         className="focus:outline-blue-dark focus:ring-1 placeholder:text-slate-400 placeholder:text-sm placeholder:font-inter w-full block rounded-lg border border-blue-light py-1 pl-3 pr-12"
